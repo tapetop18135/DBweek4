3de490f2ce1c9f8aaf58a7bfb1bb11ce18839a26
tape <a5810032@gmail.com>
2018-02-05 21:50:29 +0700

    Initial commit

---
+++ week4/public/script/main.js
@@ -0,0 +1,72 @@
+"use strict";
+var main = () => {  
+    
+    showStudents();
+    showTrascripts();
+    showCourses();
+
+}
+// function main(){
+//     showStudents();
+//     showTrascripts();
+//     showCourses();
+// }
+
+
+var showTrascripts = () => {
+    $.ajax({
+        type:"post",
+        url:"/showTrascripts",
+        data : {call:"showTrascripts"},
+        success : (data) => {
+            // str = "test"+data.length
+            var string = str(data);
+
+            $(".transcripts").html(string)
+        }
+    });
+}
+
+var showCourses = () => {
+    $.ajax({
+        type:"post",
+        url:"/showCourses",
+        data : {call:"showCourses"},
+        success : (data) => {
+            // console.log(data)
+            var string = str(data);
+
+            $(".Courses").html(string)
+            
+        }
+    });
+}
+
+
+var showStudents = () => {
+    $.ajax({
+        type:"post",
+        url:"/showStudents",
+        data : {call:"showStudents"},
+        success : (data) => {
+            var string = str(data);
+            $(".students").html(string)
+        }
+    });
+}
+
+var str = (data) => {
+    var str = "";
+    for(var i = 0 ; i < data.length ; i+= 1){
+        for(var key in data[i]){
+            str += data[i][key]+" ";
+        }
+        str += "<br>";
+    }
+    return str
+}
+
+$(main);
+
+// setInterval("main()", 500);
+// setInterval('realtime()',500);
